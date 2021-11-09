21c7b3d6e929a294d91801f7661cc4ce4d982369
PRAJKTA WAGHMARE <prajktaw24@gmail.com>
2021-11-09 10:41:27 +0530

    sonar update

---
+++ src/test/java/com/FA/Team23/OrderMs/OrderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FA.Team23.OrderMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrderMsApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
